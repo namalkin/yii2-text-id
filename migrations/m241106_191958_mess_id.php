1702bbfad08a804646cf22f056a7b41315f88a67
namalkin <nanomalkin@gmail.com>
2024-11-06 22:23:05 +0300

    Создание миграции для модели

---
+++ migrations/m241106_191958_mess_id.php
@@ -0,0 +1,42 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Class m241106_191958_mess_id
+ */
+class m241106_191958_mess_id extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        echo "m241106_191958_mess_id cannot be reverted.\n";
+
+        return false;
+    }
+
+    public function up()
+    {
+        $this->createTable('message', [
+            'id' => $this->primaryKey(),
+            'text' => $this->text()->notNull(),
+        ]);
+
+        $this->execute('ALTER TABLE message CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci');
+    }
+    
+    public function down()
+    {
+        $this->dropTable('message');
+    }
+}
